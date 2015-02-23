cfaffd0c69247e5dce6fb7994094813adf96d922
Cameron Dutro <camertron@gmail.com>
2015-02-23 00:21:27 -0800

    Add android serializer

---
+++ lib/rosette/serializers/xml-serializer.rb
@@ -0,0 +1,147 @@
+# encoding: UTF-8
+
+require 'xml-write-stream'
+require 'rosette/serializers/serializer'
+
+module Rosette
+  module Serializers
+
+    class XmlSerializer < Serializer
+      attr_reader :writer
+
+      def initialize(stream, locale, encoding = Encoding::UTF_8)
+        super
+        @writer = XmlWriteStream.from_stream(stream)
+        writer.write_header(encoding: encoding.to_s)
+        after_initialize
+      end
+
+      def after_initialize
+      end
+
+      def self.default_extension
+        '.xml'
+      end
+
+      class AndroidSerializer < XmlSerializer
+        PLURAL_FORMS = %w(
+          zero one two few many other
+        )
+
+        attr_reader :plurals, :arrays
+
+        def after_initialize
+          writer.open_tag('resources')
+          @plurals = Hash.new { |hash, key| hash[key] = {} }
+          @arrays = Hash.new { |hash, key| hash[key] = {} }
+        end
+
+        def write_raw(text)
+          stream.write(text)
+        end
+
+        def write_key_value(key, value)
+          key_parts = split_key(key)
+
+          case detect_string_type(key_parts)
+            when :plural
+              record_plural(key_parts, value)
+            when :array_element
+              record_array_element(key_parts, value)
+            else
+              write_string(key, value)
+          end
+        end
+
+        def flush
+          write_recorded_plurals
+          write_recorded_arrays
+          writer.flush
+          stream.flush
+        end
+
+        protected
+
+        def split_key(key)
+          if idx = key.rindex('.')
+            [key[0..(idx - 1)], key[(idx + 1)..-1]]
+          else
+            # this case should never happen
+            [key, '']
+          end
+        end
+
+        def record_plural(key_parts, value)
+          plurals[key_parts.first][key_parts.last] = value
+        end
+
+        def record_array_element(key_parts, value)
+          arrays[key_parts.first][key_parts.last.to_i] = value
+        end
+
+        def write_plural(name, plural_forms)
+          writer.open_tag(:plurals, name: name)
+
+          plural_forms.each_pair do |quantity, value|
+            writer.open_tag(:item, quantity: quantity)
+            write_text(value)
+            writer.close_tag
+          end
+
+          writer.close_tag
+        end
+
+        def write_array(name, array_elements)
+          writer.open_tag(:'string-array', name: name)
+          count = array_elements.keys.max
+
+          (0..count).each do |i|
+            writer.open_tag(:item)
+            write_text(array_elements[i] || '')
+            writer.close_tag
+          end
+
+          writer.close_tag
+        end
+
+        def write_recorded_plurals
+          plurals.each_pair do |name, plural_forms|
+            write_plural(name, plural_forms)
+          end
+        end
+
+        def write_recorded_arrays
+          arrays.each_pair do |name, array_elements|
+            write_array(name, array_elements)
+          end
+        end
+
+        def write_string(key, value)
+          writer.open_tag(:string, name: key)
+          write_text(value)
+          writer.close_tag
+        end
+
+        def write_text(text)
+          escaped_text = text.gsub('"', '\"')
+          writer.write_text(
+            "\"#{escaped_text}\"", escape: false
+          )
+        end
+
+        def detect_string_type(key_parts)
+          last_part = key_parts.last
+
+          if PLURAL_FORMS.include?(last_part)
+            :plural
+          elsif last_part =~ /\A[\d]+\z/
+            :array_element
+          else
+            :string
+          end
+        end
+      end
+    end
+
+  end
+end
